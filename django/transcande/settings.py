9021b4e5af3dd51c1ee8cab88e1771253194ab60
François Martineau <ffrancoismmartineau@gmail.com>
2024-01-15 14:56:30 -0500

    django postgres

--- django/transcande/settings.py
+++ django/transcande/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'transcande.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'main_db',							# choose a name for the database
+        'USER': 'your_database_user',				# an existing PostgreSQL with necessary priviledges
+        'PASSWORD': 'your_database_password',
+        'HOST': 'postgres',						# where PostgreSQL is running
+        'PORT': '5432',		
     }
 }
 
