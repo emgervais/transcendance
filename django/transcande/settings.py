50c518e7f422e9125fc6d52f77142e3376dd05c6
François Martineau <ffrancoismmartineau@gmail.com>
2024-01-15 16:22:27 -0500

    postgres setup

--- django/transcande/settings.py
+++ django/transcande/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
+# ALLOWED_HOSTS = ['172.105.98.43', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
@@ -72,15 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'transcande.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
