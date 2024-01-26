c72b854745c993e4b2ac24677a865f2569025487
Emile Gervais <egervais@c2r2p3.42quebec.com>
2024-01-25 20:04:48 -0500

    smoll adjust on templates and taking out get route(you can handle both in one route)

--- django/users/urls.py
+++ django/users/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from users import views
     
 urlpatterns = [
-    path("auth/register/", views.register, name="register"),
-    path("auth/login/", views.login, name="login"),
-    path("auth/register-post/", views.register_post, name="register_post"),
-    path("auth/login-post/", views.login_post, name="login_post"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("get-oauth-uri", views.get_oauth_uri, name="get_oauth_uri"),
 
     #path("get-oauth-uri/", views.get_oauth_uri, name="get_oauth_uri"),
 ]
--- django/users/views.py
+++ django/users/views.py
@@ -14,45 +14,43 @@ from users import oauth42
 class HtmxHttpRequest(HttpRequest):
     htmx: HtmxDetails
 
-@require_GET
 def register(request: HtmxHttpRequest) -> HttpResponse:
-    print(request.htmx)
-    print(request)
-    return render(request, 'auth/register.html', {'form': RegisterForm()})
-
-@require_GET
-def login(request: HtmxHttpRequest) -> HttpResponse:
-    print(request.htmx)
-    print(request)
-    form = LoginForm()
-    return render(request, 'auth/login.html', {'form': form, 'logged': request.user.is_authenticated})
-
-@require_POST
-def register_post(request: HtmxHttpRequest) -> HttpResponse:
-    form = RegisterForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username=form.cleaned_data.get('username')
-        authenticate(username=username, password=form.cleaned_data.get('password1'))
-        messages.success(request, f'Account created for {username}!')
-        return redirect(reverse('index'))
-    return render(request, 'auth/register.html', {'form': form})
-
-@require_POST
-def login_post(request: HtmxHttpRequest) -> HttpResponse:
-    form = LoginForm(request, request.POST)
-    if form.is_valid():
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            messages.success(request, f'You are now logged in!')
-            return render(request, 'auth/login.html', {'form': form, 'logged': request.user.is_authenticated})
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            user = form.save()
+            messages.success(request, f'Account created for {username}!')
+            return render(request, 'index.html')
         else:
-            form.add_error(None, "Wrong email or password.")
-    return render(request, 'auth/login.html', {'form': form, 'logged': request.user.is_authenticated})
+            return render(request, 'auth/register.html', {'form': form})
+    else:
+        form = RegisterForm()
+        return render(request, 'auth/register.html', {'form': form})
+
+def login(request: HtmxHttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                messages.success(request, f'You are now logged in!')
+                return render(request, 'index.html', {'form': form, 'logged': request.user.is_authenticated})
+            else:
+                form.add_error(None, "Wrong email or password.")
+        return render(request, 'auth/login.html', {'form': form, 'logged': request.user.is_authenticated})
+    else:
+        form = LoginForm()
+        return render(request, 'auth/login.html', {'form': form, 'logged': request.user.is_authenticated})
 
 @require_POST
 def logout(request: HtmxHttpRequest) -> HttpResponse:
     request.session.flush()
     return redirect(reverse('index'))
+
+def get_oauth_uri(request):
+    redirect_uri = "https://localhost"
+    url = oauth42.create_oauth_uri(redirect_uri)
+    return JsonResponse(url, safe=False)
