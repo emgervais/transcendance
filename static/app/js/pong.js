88833d7b1876c417ac40932f954bf40931a23cac
Elijah Boyce-Nadeau <eboyce-n@c2r3p6.42quebec.com>
2024-01-29 19:06:20 -0500

    testing websockets

--- static/app/js/pong.js
+++ static/app/js/pong.js
@@ -3,6 +3,8 @@ var canvas = document.getElementById("pong-canvas");
 var ctx = canvas.getContext("2d");
 
 var playerid = 0;
+var playery = 0;
+var ff = 1.56;
 
 ws.onopen = function (event) {
     console.log("Websocket connection opened.");
@@ -21,8 +23,10 @@ ws.onmessage = function (event) {
 }
 
 function ping() {
-    ws.send(JSON.stringify({
-        type: "ping",
-        message: "ping"
-    }));
+    let a = new ArrayBuffer(8);
+    playery += 16;
+    let v = new DataView(a);
+    v.setUint32(0, playery, true);
+    v.setFloat32(4, ff, true);
+    ws.send(a);
 }
