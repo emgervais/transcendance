6c257fd2d42b90fb4ada51182431cd593a8ea9e5
Emile Le Sage <ele-sage@c2r3p5.42quebec.com>
2024-02-07 07:46:59 -0500

    more cleanup

--- django/transcendence/urls.py
+++ django/transcendence/urls.py
@@ -1,30 +1,6 @@
-"""
-URL configuration for transcendence project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import include, path
-from rest_framework import routers
-from users import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-
 
 urlpatterns = [
     path('', include('app.urls')),
     path('', include('users.urls')),
-    path('', include(router.urls)),
-    path('users/login/', views.login, name='login'),
 ]
