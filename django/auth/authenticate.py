56d1519cf927d8a08a57d8d643aa49a2b68d22b5
François Martineau <ffrancoismmartineau@gmail.com>
2024-02-20 12:34:41 -0500

    backend

--- django/auth/authenticate.py
+++ django/auth/authenticate.py
@@ -10,12 +10,7 @@ class CustomAuthentication(JWTAuthentication):
         if request.path in ignore_paths:
             return None
         
-        header = self.get_header(request)
-        
-        if header is None:
-            raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE']) or None
-        else:
-            raw_token = self.get_raw_token(header)
+        raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE']) or None
         if raw_token is None:
             return None
 
