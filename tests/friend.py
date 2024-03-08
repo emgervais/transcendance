091fda18b288ad07ab0628ea7df16783d6961ffa
François Martineau <ffrancoismmartineau@gmail.com>
2024-03-08 11:47:27 -0500

    search endpoint and assertions

--- tests/friend.py
+++ tests/friend.py
@@ -1,5 +1,6 @@
 import endpoints
 from auth import auth
+import auth as auth_module
 
 @auth(2)
 def friend_requests_wrong(users):
@@ -77,6 +78,62 @@ def init_friends(users):
 def init_friend_requests(users):
     for user in users[1:]:
         response = endpoints.make_friend_request(user["cookies"], users[0]["id"])
+
+
+
+@auth(3)
+def search_user(users):
+    def accept_friend_request(user):
+        response = endpoints.friend_requests(user["cookies"])
+        request_id = response["data"][0]["id"]
+        endpoints.friend_request(user["cookies"], request_id, "put")
+
+    user1, user2, user3 = users
+
+    response = endpoints.search(user1["cookies"], auth_module.user["username"])
+    assert len(response["data"]) == 2, "search_user: no params"
+    response = endpoints.search(user1["cookies"], auth_module.user["username"], is_friend=True)
+    assert len(response["data"]) == 0
+
+    endpoints.make_friend_request(user1["cookies"], user2["id"])
+    accept_friend_request(user2)
+    response = endpoints.search(user1["cookies"], auth_module.user["username"], is_friend=True)
+    assert len(response["data"]) == 1, "search_user: is_friend"
+
+    endpoints.make_friend_request(user1["cookies"], user3["id"])
+    response = endpoints.search(user1["cookies"], auth_module.user["username"], friend_request_sent=True)
+    assert len(response["data"]) == 1, "search_user: friend_request_sent"
+    response = endpoints.search(user1["cookies"], auth_module.user["username"], friend_request_received=True)
+    assert len(response["data"]) == 0, "search_user: friend_request_received"
+
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], friend_request_sent=True)
+    assert len(response["data"]) == 0, "search_user: friend_request_sent"
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], friend_request_received=True)
+    assert len(response["data"]) == 1, "search_user: friend_request_received"
+
+    accept_friend_request(user3)
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], friend_request_sent=True)
+    assert len(response["data"]) == 0, "search_user: friend_request_sent"
+    response = endpoints.search(user1["cookies"], auth_module.user["username"], friend_request_received=True)
+    assert len(response["data"]) == 0, "search_user: friend_request_received"
+
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], friend_request_sent=True)
+    assert len(response["data"]) == 0, "search_user: friend_request_sent"
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], friend_request_received=True)
+    assert len(response["data"]) == 0, "search_user: friend_request_received"
+
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], is_friend=True)
+    assert len(response["data"]) == 1, "search_user: is_friend"
+
+    endpoints.block_user(user3["cookies"], user1["id"])
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], got_blocked=True)
+    assert len(response["data"]) == 0, "search_user: got_blocked"
+    response = endpoints.search(user3["cookies"], auth_module.user["username"], is_blocked=True)
+    assert len(response["data"]) == 1, "search_user: is_blocked"
+    response = endpoints.search(user1["cookies"], auth_module.user["username"], is_blocked=True)
+    assert len(response["data"]) == 0, "search_user: is_blocked"
+    response = endpoints.search(user1["cookies"], auth_module.user["username"], got_blocked=True)
+    assert len(response["data"]) == 1, "search_user: got_blocked"
 
 
 
