17bf072e95a44af99f40c94c0e9e8dc2d6e37143
François Martineau <ffrancoismmartineau@gmail.com>
2024-03-08 13:22:09 -0500

    startMatch and stuff

--- static/js/router/router.js
+++ static/js/router/router.js
@@ -2,7 +2,6 @@ import * as account from "/js/account/account.js";
 import * as nav from "/js/nav.js";
 import * as pong from "/js/pong/pong.js";
 import { equipParamRoutes, setParams } from "/js/router/params.js";
-import { getParams } from "/js/router/params.js";
 import { displayCurrUser } from "/js/user/currUser.js";
 
 const routes = equipParamRoutes({
