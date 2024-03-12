a44bb4ddfdbab11af744238e61d2d145dfb96cc6
Emile Le Sage <ele-sage@c2r2p1.42quebec.com>
2024-03-12 16:22:26 -0400

    bug fixes

--- django/notification/consumers.py
+++ django/notification/consumers.py
@@ -138,7 +138,7 @@ class NotificationConsumer(AsyncWebsocketConsumer):
 
     async def search_match(self, room):
         if room != 'global':
-            if await self.send_match_request(room):
+            if not await self.send_match_request(room):
                 print("cancel")
                 return
         await self.send(text_data=json.dumps({
@@ -177,31 +177,31 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         opponent = await get_user(opponent_id)
         if opponent:
             opponent_channel = await get_main_channel(opponent_id, True)
-            if opponent.status == 'online' and matchmaking_redis.zrank(room, opponent_id) is None:
-                if opponent_channel:
-                    await self.channel_layer.send(opponent_channel, {
-                        'type': 'send.notification',
-                        'notification': 'pong',
-                        'description': 'matchRequest',
-                        'room': room,
-                        'userId': self.user.id
-                    })
-                    return True
+            if opponent.status == 'online':
+                if matchmaking_redis.zrank(room, opponent_id) is None:
+                    print('Sending match request')
+                    if opponent_channel:
+                        await self.channel_layer.send(opponent_channel, {
+                            'type': 'send.notification',
+                            'notification': 'pong',
+                            'description': 'matchRequest',
+                            'room': room,
+                            'userId': self.user.id
+                        })
+                return True
             elif opponent.status == 'in-game':
                 await self.send(text_data=json.dumps({
                     'type': 'pong',
                     'description': 'opponentInGame',
                     'userId': opponent_id
                 }))
-                return False
             else:
                 await self.send(text_data=json.dumps({
                     'type': 'pong',
                     'description': 'opponentOffline',
                     'userId': opponent_id
                 }))
-                return False
-        return True
+        return False
         
 # Helper functions
 def user_disconnect(user):
